07ccd0f3fbb00ec27a8b0a76c14056b1c8a97052
gek169 <gek169>
2021-05-12 01:02:14 -0400

    Automatic commit.

--- lua/luaconf.h
+++ lua/luaconf.h
@@ -117,9 +117,10 @@
 @@ LUA_32BITS enables Lua with 32-bit integers and 32-bit floats.
 */
 
-//MARK: GEK MODIFIED THIS!!!!
+/*#define LUA_32BITS	0
 
-//#define LUA_32BITS	0
+MARK: GEK MODIFIED THIS!!!!
+*/
 #define LUA_32BITS	1
 
 
